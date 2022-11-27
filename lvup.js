830fe2b40345a9f80f346089005ef7bff7bf8f00
WonDuJin <windyjan1@naver.com>
2022-11-27 23:42:09 +0900

    레벨업시 경험치 초기화를 위해 경험치 / 레벨업 따로 구분. 이미지 추가

--- lvup.js
+++ lvup.js
@@ -10,34 +10,62 @@ root.style.flexDirection =  "culumn"
 
 button.style.width="300px";
 button.style.height="500px";
-button.style.backgroundColor = "#808080"
+button.style.textAlign = "center"
 
 exp.style.width="300px";
 exp.style.height="500px";
-exp.style.fontSize = "50px"
-exp.style.innerHTML = "Exp :"
+exp.style.fontSize = "40px"
 
-monster.style.width="100px"
-monster.style.height = "100px"
-monster.style.backgroundColor = "red"
+
+monster.style.width="200px"
+monster.style.height = "200px"
+monster.style.textAlign = "center"
 
 
 //버튼을 클릭했을 시 이벤트 발생
+// button.addEventListener("click",()=>{
+//   let count= 0
+  //일일히 클릭하는게 힘들어서 인터벌을 주었음
+//     setInterval(()=>{
+//     count ++
+    //Lv이 100일때 
+//     for (let i = 0; i <= 100; i++){
+//       console.log(count)
+      // 조건문으로 10의 제곱근을 추가하여 경험치를 구현      
+//       if(count === 10**i){        
+//         alert("Lv"+(i+1))
+//       }      
+//     }
+//   },100)
+// });
+
+
+
+// 레벨업시 경험치를 초기화 되는 것을 구현하기위해 경험치 획득과 레벨업 획들을 구분해놨음
+let count= 0
+let maxexp = 1
+let level = 1
+
+// 경험치 증가
 button.addEventListener("click",()=>{
-  let count= 0
   //일일히 클릭하는게 힘들어서 인터벌을 주었음
     setInterval(()=>{
-    count ++
-    //Lv이 100일때 
-    for (let i = 0; i <= 100; i++){
+      count ++
       console.log(count)
-      // 조건문으로 10의 제곱근을 추가하여 경험치를 구현      
-      if(count === 10**i){        
-        alert("Lv"+(i+1))
-      }      
+      //Lv이 100일때 
+    },100)  
+  })
+
+// 레벨업
+button.addEventListener("click",()=>{
+  setInterval(()=>{
+    if(count >= maxexp){
+      console.log("레벨업")
+      count = 0
+      maxexp =  maxexp * 10
+      level++
     }
+    exp.innerText = "Exp : " + count + " / " + maxexp  + "\n Lv : " + level
   },100)
-});
-
-
+})
 
